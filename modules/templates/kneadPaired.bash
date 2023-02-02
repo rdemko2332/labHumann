bea37692108b74d0fd049bb5dd4daf963a14ae94
rdemko2332 <rdemko2332@gmail.com>
2023-02-02 12:28:01 -0500

    Adding bowtie2 path parameter

--- modules/templates/kneadPaired.bash
+++ modules/templates/kneadPaired.bash
@@ -5,6 +5,7 @@ ${params.kneaddataCommand} \
    -i1 ${id}_R1_001.fastq.gz \
    -i2 ${id}_R2_001.fastq.gz \
    --cat-final-output \
+   --bowtie2 /usr/bin/bowtie2 \
    -o .
 mv ${id}_R1_001_kneaddata.fastq ${id}_1_kneaddata.fastq
 
