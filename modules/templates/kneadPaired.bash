5d1e224cebec1a712207beb4d3b85b98e7fd1f99
rdemko2332 <rdemko2332@gmail.com>
2023-02-06 10:55:09 -0500

    Changing bowtie2 path declaration

--- modules/templates/kneadPaired.bash
+++ modules/templates/kneadPaired.bash
@@ -5,7 +5,7 @@ ${params.kneaddataCommand} \
    -i1 ${id}_R1_001.fastq.gz \
    -i2 ${id}_R2_001.fastq.gz \
    --cat-final-output \
-   --bowtie2 /usr/bin/bowtie2 \
+   --bowtie2 /usr/bin/ \
    -o .
 mv ${id}_R1_001_kneaddata.fastq ${id}_1_kneaddata.fastq
 
